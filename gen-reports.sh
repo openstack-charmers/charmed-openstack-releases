a38f7e7bb2f3a61f25ef6545f51206bfa368611e
Felipe Reyes <felipe.reyes@canonical.com>
2022-12-01 11:30:26 -0300

    Generate report for published charms in Charmhub
    There are 2 categories of reports:
    
    - 'builds' will contain the reports associated to Launchpad builds,
      where the main consumer of them is the development team that needs to
      track if/when a build has been made.
    - 'charmhub' will contain the reports associted to the charms available
      to the users.

--- gen-reports.sh
+++ gen-reports.sh
@@ -10,4 +10,15 @@ charmhub-lp-tool \
   -f html \
   check-builds \
   --channel={train,ussuri,victoria,wallaby,xena,yoga,zed,latest,luminous,mimic,nautilus,octopus,pacific,quincy,20.03,20.12,21.09,22.03,22.09}/{edge,beta,candidate,stable} \
-  --output ./report/
+  --output ./report/builds/
+
+charmhub-lp-tool \
+  --log $LOG_LEVEL \
+  --config-dir ./release-tools/lp-builder-config/ \
+  -p ovn \
+  -p ceph \
+  -p openstack \
+  -f html \
+  charmhub-report \
+  --track={train,ussuri,victoria,wallaby,xena,yoga,zed,latest,luminous,mimic,nautilus,octopus,pacific,quincy,20.03,20.12,21.09,22.03,22.09} \
+  --output ./report/charmhub/
