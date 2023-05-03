14ee01d11aa98e99c221f95a881db8407624e25a
Felipe Reyes <felipe.reyes@canonical.com>
2023-05-03 11:33:42 +0200

    Drop explicit list of channels and tracks.
    Dropping the explicit list of tracks and channels allows
    charmhub-lp-tools to use all the defined ones in lp-builder-config
    making the maintenance easier.

--- gen-reports.sh
+++ gen-reports.sh
@@ -9,7 +9,6 @@ charmhub-lp-tool \
   -p openstack \
   -f html \
   check-builds \
-  --channel={train,ussuri,victoria,wallaby,xena,yoga,zed,latest,luminous,mimic,nautilus,octopus,pacific,quincy,20.03,20.12,21.09,22.03,22.09}/{edge,beta,candidate,stable} \
   --output ./report/builds/
 
 charmhub-lp-tool \
@@ -20,5 +19,4 @@ charmhub-lp-tool \
   -p openstack \
   -f html \
   charmhub-report \
-  --track={train,ussuri,victoria,wallaby,xena,yoga,zed,latest,luminous,mimic,nautilus,octopus,pacific,quincy,20.03,20.12,21.09,22.03,22.09} \
   --output ./report/charmhub/
